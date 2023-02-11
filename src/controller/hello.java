aeee01ca593b04412acf9fac00c141d51e93b575
zzy <1364542816@qq.com>
2023-02-11 17:02:04 +0800

    zxc

---
+++ src/controller/hello.java
@@ -0,0 +1,14 @@
+package controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Sunrise
+ * @Date: 2023/02/11/16:59
+ * @Description:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
